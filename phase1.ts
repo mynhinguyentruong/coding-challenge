ec4cd7c751ba38f6cdf2264cca77498083226d96
mynhinguyentruong <mynhinguyentruong@gmail.com>
2022-09-17 16:57:32 -0400

    Refactor duplicate logic

---
+++ phase1.ts
@@ -0,0 +1,34 @@
+import axios from "axios";
+import { goalEndPoint, candidateId } from './index'
+
+axios.defaults.baseURL = "https://challenge.crossmint.io/api";
+
+const getData = async (url: string, candidateId: string) => {
+  try {
+    const response = await axios.get(url);
+    console.log(response.data.goal);
+    const matrix = response.data.goal;
+    for (let row = 0; row < matrix.length; row++) {
+      for (let column = 0; column < matrix[row].length; column++) {
+        // if the value match , make a POST request
+        if (matrix[row][column] === "POLYANET") {
+ 
+          // await axios
+          //   .delete("/polyanets", { data: {
+          //     row,
+          //     column,
+          //     candidateId,
+          //   }})
+          await axios
+            .post('/polyanets', {row, column, candidateId})
+            .then(() => console.log('successful'))
+            .catch(err => console.log(err))
+        }
+      }
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+getData(goalEndPoint, candidateId);
--- phase2.ts
+++ phase2.ts
@@ -1,9 +1,7 @@
 import axios from "axios";
+import { goalEndPoint, candidateId } from './index'
 
 axios.defaults.baseURL = "https://challenge.crossmint.io/api";
-
-const goalEndPoint = "map/54d4b7e9-642f-41aa-93fc-e705f35fa40e/goal";
-const candidateId = "54d4b7e9-642f-41aa-93fc-e705f35fa40e";
 
 const getData = async (url: string, candidateId: string) => {
   try {
